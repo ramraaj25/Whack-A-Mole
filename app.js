5b180adc4e74c21f071e1b903bb6f2f7c6b19f15
ramraaj25 <ramraaj.bishnoie@gmail.com>
2020-07-22 20:03:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+    molePosition = randomPosition.id
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if (id.id === molePosition) {
+            result = result + 1
+            score.textContent = result
+            molePosition = null
+        }
+    })
+})
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime === 0) {
+        clearInterval(timerId)
+        alert('Game Over! Your final score is' + result)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
